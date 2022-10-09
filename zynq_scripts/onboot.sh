a13ec6674f3b3c2a43400a8d790c179962453d19
Jack <jack@realtimeradio.co.uk>
2022-10-09 15:28:42 +0000

    Add onboot script
    Meant to be run when sparrow boots

---
+++ zynq_scripts/onboot.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SPARROW_FPGA_PATH=/home/casper/src/sparrow-fpga
+SIFILE=Si5332-GM1-RevD-SPRW1-Registers-extref-pllcmos_in_phase50M_VCTCXOinput.txt
+PYTHON=/home/casper/python3-venv/bin/python
+
+cd $SPARROW_FPGA_PATH
+cd adc_tests
+
+echo "Programming Si5332 with $SIFILE"
+$PYTHON program_i2c.py $SIFILE > /dev/null
+
+# The following requires that the FPGA be programmed
+#echo "Programming LMX"
+#python pll_config.py > /dev/null
